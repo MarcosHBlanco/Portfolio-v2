34a9399e54387ac55e6228d2cc22e2f68f0186e4
MarcosHBlanco <124410355+MarcosHBlanco@users.noreply.github.com>
2025-10-13 22:27:53 -0700

    Removed Unutilized component

--- portfolio-v2/src/components/Header.jsx
+++ portfolio-v2/src/components/Header.jsx
@@ -1,24 +1,24 @@
-import githubLogo from "../assets/github64px.png";
-import linkedinLogo from "../assets/linkedin64px.png";
+// import githubLogo from "../assets/github64px.png";
+// import linkedinLogo from "../assets/linkedin64px.png";
 
-export default function Header() {
-	return (
-		<header className="flex flex-col">
-			<div>
-				<ul className="flex w-26">
-					<li className="m-2">
-						<a href="https://github.com/MarcosHBlanco" target="_blank">
-							<img src={githubLogo} loading="lazy" alt="Github logo" />
-						</a>
-					</li>
-					<li className="m-2">
-						<a href="https://www.linkedin.com/in/marcoshblanco/">
-							<img src={linkedinLogo} loading="lazy" alt="Linkedin logo" />
-						</a>
-					</li>
-				</ul>
-			</div>
-			<h1 className="text-cyan-300 justify-around">Marcos Blanco</h1>
-		</header>
-	);
-}
+// export default function Header() {
+// 	return (
+// 		<header className="flex flex-col">
+// 			<div>
+// 				<ul className="flex w-26">
+// 					<li className="m-2">
+// 						<a href="https://github.com/MarcosHBlanco" target="_blank">
+// 							<img src={githubLogo} loading="lazy" alt="Github logo" />
+// 						</a>
+// 					</li>
+// 					<li className="m-2">
+// 						<a href="https://www.linkedin.com/in/marcoshblanco/">
+// 							<img src={linkedinLogo} loading="lazy" alt="Linkedin logo" />
+// 						</a>
+// 					</li>
+// 				</ul>
+// 			</div>
+// 			<h1 className="text-cyan-300 justify-around">Marcos Blanco</h1>
+// 		</header>
+// 	);
+// }
